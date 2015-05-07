2cd825a123c0fc7fbe87b91e4ded8e8d2136e6dc
Esther Greatbanks & Peter Jacobson <pair+estherlg1.peterjacobson@devbootcamp.com>
2015-05-07 14:56:56 +1200

    user & user connection migrations

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,6 @@
 class User < ActiveRecord::Base
   has_many :tweets
   has_many :user_connections
-  has_many :followers ??
-  has_many :following ??
+  has_many :followers through :user_connections foreign_key :followee_id
+  has_many :followees through :user_connections foreign_key :follower_id
 end
