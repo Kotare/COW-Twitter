37cced20dd73ce05604e0e1ce3944446b2fc7a64
Jonathan Bolo <jb.bolo@gmail.com>
2015-05-07 15:56:03 +1200

    add first controller tests

---
+++ spec/twitter_controller_spec.rb
@@ -0,0 +1,25 @@
+require 'spec_helper'
+require 'uri'
+
+describe "TwitterController" do
+
+  describe "GET '/'" do
+
+    before do
+      get '/'
+    end
+
+    it "returns an HTTP status code of 200" do
+      expect(last_response.status).to eq(200)
+    end
+
+    it  "renders the sign-in / sign-up view" do
+      expect(last_response.body).to include("Password")
+    end
+
+  end
+
+  # describe "POST '/sign_in'" do
+  # end
+
+end
