2cc73844a486af321b732a07b51bd27781f797d6
Esther Greatbanks & Peter Jacobson <pair+estherlg1.peterjacobson@devbootcamp.com>
2015-05-07 19:15:00 +1200

    testing updates including session info

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,6 +1,12 @@
 
 get '/users' do
   @users = User.all
+  @current_user = User.find(session[:user])
   erb :users
+end
+
+post '/users/follow/:user_id' do
+  Connection.create(follower_id: session[:user], followee_id: params[:user_id])
+  redirect "/users"
 end
 
