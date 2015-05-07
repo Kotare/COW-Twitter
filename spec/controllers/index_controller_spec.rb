8e46a4feef4600f8801a09f246a3719bc8ebbad6
Jonathan Bolo <jb.bolo@gmail.com>
2015-05-07 18:06:51 +1200

    additional tests

--- spec/controllers/index_controller_spec.rb
+++ spec/controllers/index_controller_spec.rb
@@ -13,13 +13,32 @@ describe "IndexController" do
       expect(last_response.status).to eq(200)
     end
 
-    it  "renders the sign-in / sign-up view" do
+    it "renders the sign-in / sign-up view" do
       expect(last_response.body).to include("password")
     end
 
   end
 
-  # describe "POST '/sign_in'" do
-  # end
+  describe "POST '/sign_in'" do
+
+    before do
+      @user = User.create(username: Faker::Name.name, email: Faker::Internet.email, password: Faker::Internet.password)
+
+      @valid_params = {username: @user.username, password: @user.password}
+      post '/sign_in', @valid_params
+    end
+
+    it "returns an HTTP status code of 200" do
+      expect(last_response.status).to eq(200)
+    end
+
+    # it "renders the index page" do
+    #   expect(last_response.body).to include("Logout:")
+    # end
+
+    # after do
+    #   User.destroy_all
+    # end
+  end
 
 end
