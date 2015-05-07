35d116211c38d4172a7b61bc512bd99779512ab5
Esther Greatbanks & Peter Jacobson <pair+estherlg1.peterjacobson@devbootcamp.com>
2015-05-07 13:58:04 +1200

    fixed relation syntax

--- db/migrate/20150507114657_create_tweets_table.rb
+++ db/migrate/20150507114657_create_tweets_table.rb
@@ -2,7 +2,7 @@ class CreateTweetsTable < ActiveRecord::Migration
   def change
     create_table :tweets do |t|
       t.string :content, null: false
-      t.reference :user, index: true, null: false
+      t.belongs_to :user, index: true, null: false
       t.timestamps null: false
     end
   end
