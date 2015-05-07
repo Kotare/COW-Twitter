427ff23f11d70752e41c8fd8ed4e48778f612d03
Esther Greatbanks & Peter Jacobson <pair+estherlg1.peterjacobson@devbootcamp.com>
2015-05-07 15:11:29 +1200

    rejig migrations according to medium/@.../.. e0992c27c1e

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,10 @@
 class User < ActiveRecord::Base
   has_many :tweets
   has_many :user_connections
-  has_many :followers through :user_connections foreign_key :followee_id
-  has_many :followees through :user_connections foreign_key :follower_id
+
+  has_many :followers, through: :follower_connections, source: :follower#:user_connections foreign_key :followee_id
+  has_many :follower_connections, foreign_key: :followee_id, class_name: "Connection"
+
+  has_many :followees, through: :follower_connections, source: :follower# through :user_connections foreign_key :follower_id
+  has_many :followee_connections, foreign_key: :follower_id, class_name: "Connection"
 end
